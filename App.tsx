896517b986013a02f92b1ebc88f5c0981d939e8a
AhmadKhalil05 <s12323576@stu.najah.edu>
2026-03-10 12:27:04 +0200

    edited app.tsx text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your </Text>
+      <Text>show your temp component here </Text>
       <StatusBar style="auto" />
     </View>
   );
