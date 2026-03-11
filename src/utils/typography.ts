40b8e8d6341b4135558b85c215b168041da1becf
AhmadKhalil05 <s12323576@stu.najah.edu>
2026-03-12 01:41:57 +0200

    added fonts & app icon

---
+++ src/utils/typography.ts
@@ -0,0 +1,20 @@
+export const Family = {
+    
+    // Founders Grotesk
+    FG_Bold: 'FoundersGrotesk-Bold',
+    FG_BoldItalic: 'FoundersGrotesk-BoldItalic',
+    FG_Light: 'FoundersGrotesk-Light',
+    FG_LightItalic: 'FoundersGrotesk-LightItalic',
+    FG_Medium: 'FoundersGrotesk-Medium',
+    FG_MediumItalic: 'FoundersGrotesk-MediumItalic',
+    FG_Regular: 'FoundersGrotesk-Regular',
+    FG_RegularItalic: 'FoundersGrotesk-RegularItalic',
+    FG_Semibold: 'FoundersGrotesk-Semibold',
+    FG_SemiboldItalic: 'FoundersGrotesk-SemiboldItalic',
+    FG_Cond_Lt: 'FoundersGroteskCond-Lt',
+    FG_XCond_Bold: 'FoundersGroteskXCond-Bold',
+
+    // Helvetica
+    HV_Bold: 'Helvetica-Bold',
+    HV_Regular: 'Helvetica',
+} as const;
