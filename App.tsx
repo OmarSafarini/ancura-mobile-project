20d81d8f41b96cd445042abcafef85751f27cbab
Omar Safarini <omarsafarini123344@gmail.com>
2026-03-13 16:51:46 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,42 +4,6 @@ import CaseCard from './src/components/common/CaseCard';
 export default function App() {
   return (
     <View>
-       <CaseCard
-      data={{
-        id: 1,
-        patient_id: 22,
-        title: "Severe headache for 3 days",
-        description: "Pain in the left side of the head",
-        created_at: "2h ago",
-        file: "",
-        status: 'Doctor Replied',
-        isEmergency: true,
-      }}
-    />
-     {/* <CaseCard
-      data={{
-        id: 1,
-        patient_id: 22,
-        title: "Severe headache for 3 days",
-        description: "Pain in the left side of the head",
-        created_at: "2h ago",
-        file: "",
-        status: 'Resolved',
-        isEmergency: true,
-      }}
-    />
-     <CaseCard
-      data={{
-        id: 1,
-        patient_id: 22,
-        title: "Severe headache for 3 days",
-        description: "Pain in the left side of the head",
-        created_at: "2h ago",
-        file: "",
-        status: 'Under Review',
-        isEmergency: true,
-      }}
-    /> */}
     </View>
   );
 }
