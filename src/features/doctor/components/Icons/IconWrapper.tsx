63f0f63e16c5fc4e88959ae86ea364b8079f75ec
aprar-jalal <s12324682@stu.najah.edu>
2026-03-25 15:00:46 -0700

    added border

--- src/features/doctor/components/Icons/IconWrapper.tsx
+++ src/features/doctor/components/Icons/IconWrapper.tsx
@@ -5,6 +5,7 @@ type Props = {
   size: number;
   bgColor?: string;
   shape?: "circle" | "square";
+  border?:string;
   //so the icon will be in the shape
   children: React.ReactNode;
 };
@@ -13,22 +14,25 @@ export default function IconWrapper({
   size,
   bgColor = "#eee",
   shape = "circle",
+  border='#6D7EB5',
   children,
 }: Props) {
   return (
-    <View style={[styles.bg, dynamicStyle(size, bgColor, shape)]}>
+    <View style={[styles.bg, dynamicStyle(size, bgColor, shape,border)]}>
       {/*any thing inside the wrapper will be shown here */}
       {children}
     </View>
   );
 }
 
-const dynamicStyle = (size: number, bgColor: string, shape: "circle" | "square") => ({
+const dynamicStyle = (size: number, bgColor: string, shape: "circle" | "square",border:string) => ({
   width: size,
   height: size,
   //16 according to figma
   borderRadius: shape === "circle" ? size / 2 : 16, 
   backgroundColor: bgColor,
+  borderColor:border,
+  borderWidth:1
 });
 
 const styles = StyleSheet.create({
