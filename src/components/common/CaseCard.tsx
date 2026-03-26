054c8c3a5f3fbf0454979bc43a085b509ad98f66
Omar Safarini <omarsafarini123344@gmail.com>
2026-03-26 17:49:39 +0200

    fixing some layout problems for the Case Card

--- src/components/common/CaseCard.tsx
+++ src/components/common/CaseCard.tsx
@@ -1,132 +1,113 @@
 import { Colors } from "@/utils/colors";
+import { Family } from "@/utils/typography";
 import { Ionicons } from "@expo/vector-icons";
-// import { useRouter } from "expo-router";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import { moderateScale, scale, verticalScale } from "react-native-size-matters";
+import { scale } from "@/utils/responsive";
+import { CaseData } from "@/types/CaseData";
+import UnderViewIcon from "@/assets/icons/UnderViewIcon";
+import DoctorRepliedIcon from "@/assets/icons/DoctorRepliedIcon";
+import ResolvedIcon from "@/assets/icons/ResolvedIcon";
 
-type CaseStatus = "Under Review" | "Doctor Replied" | "Resolved";
-type CaseData = {
-  id: number;
-  patient_id: number;
-  title: string;
-  description: string;
-  created_at: string;
-  file?: string;
-  status: CaseStatus;
-  isEmergency: boolean;
-};
 type Props = {
   data: CaseData;
 };
 
 const STATUS_CONFIG = {
-  "Under Review": {
+  "Under Review": { 
     containerColor: "rgba(232,112,0 , 0.24)",
-    icon: "time-outline",
-    iconBackground: Colors.underReview,
+    IconComponent: UnderViewIcon, 
+    iconBackground: Colors.underReview, 
   },
-  "Doctor Replied": {
-    containerColor: "rgba(0, 0, 0, 0.13)",
-    icon: "chatbubbles-outline",
-    iconBackground: Colors.secondary,
+    "Doctor Replied": { 
+    containerColor: "rgba(8, 7, 14 , 0.13)", 
+    IconComponent: DoctorRepliedIcon, 
+    iconBackground: Colors.secondary, 
   },
-  "Resolved": {
-    containerColor: Colors.secondaryLight,
-    icon: "checkmark-outline",
-    iconBackground: Colors.secondary,
+  "Resolved": { 
+    containerColor: Colors.secondaryLight, 
+    IconComponent: ResolvedIcon, 
+    iconBackground: Colors.secondary, 
   },
-} as const;
+  "Empty": { 
+    containerColor: "rgba(8, 7, 14 , 0.13)", 
+  }
+};
 
 export default function CaseCard({ data }: Props) {
-
-  // const router = useRouter();
-  const statusConfig = STATUS_CONFIG[data.status];
+  const status = data.status || "Empty";
+  const config = STATUS_CONFIG[status];
 
   return (
-   <Pressable
-    style={[
-      styles.container,
-      { backgroundColor: statusConfig.containerColor },
-      data.isEmergency && {
-        shadowColor: "rgba(255, 0, 0, 0.65)", 
-        shadowOffset: { width: 0, height: 0 }, 
-        shadowOpacity: 0.5,
-        shadowRadius: 8,
-        elevation: 8, // for the Android obicity
-      }
-    ]}
-    // onPress={() => router.push(`/case/${data.id}`)}
-  >
-      
+    <Pressable style={[styles.container,{ backgroundColor: config.background },data.isEmergency && styles.emergencyShadow,]}>
       <Text style={styles.title}>{data.title}</Text>
       <View style={styles.arrowContainer}>
-        <Ionicons name="chevron-forward" size={moderateScale(16)} />
+        <Ionicons name="chevron-forward" size={scale(16)} />
       </View>
-
       <View style={styles.bottomRow}>
         <Text style={styles.time}>{data.created_at}</Text>
-        <View style={styles.reviewContainer}>
-          <View style={[styles.iconCircle, { backgroundColor: statusConfig.iconBackground }]}>
-            <Ionicons name={statusConfig.icon} size={moderateScale(10)} color="white"/>
+        {status !== "Empty" && config.Icon && (
+          <View style={styles.reviewContainer}>
+            <View style={[styles.iconCircle, { backgroundColor: config.iconColor }]}>
+              <config.Icon width={scale(10)} height={scale(10)} />
+            </View>
+            <Text style={[styles.reviewText, { color: config.iconColor }]}>{status}</Text>
           </View>
-          <Text style={[styles.reviewText,{ color: statusConfig.iconBackground },]}>
-            {data.status}
-          </Text>
-        </View>
+        )}
       </View>
-
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: scale(140),
-    height: verticalScale(120),
+    width: scale(160),
+    height: scale(130),
     borderRadius: scale(15),
     padding: scale(12),
     justifyContent: "space-between",
   },
-
+  emergencyShadow: {
+    shadowColor: "rgba(255,0,0,0.85)",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 8,
+    elevation: 8,
+  },
   title: {
-    fontSize: moderateScale(9),
-    fontWeight: "700",
+    fontSize: scale(9),
+    fontFamily: Family.FG_Semibold,
     color: Colors.textDark,
     maxWidth: "75%",
   },
-
+  arrowContainer: {
+    backgroundColor: Colors.formBackground,
+    alignSelf: "center",
+    borderRadius: scale(20),
+    padding: scale(5),
+  },
   bottomRow: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
-
-  arrowContainer: {
-    backgroundColor: "#fff",
-    alignSelf: "center",
-    borderRadius: scale(20),
-    padding: scale(2),
-  },
-
   time: {
-    fontSize: moderateScale(7),
+    fontSize: scale(7),
+    fontFamily: Family.HV_Regular,
     color: "rgba(0,0,0,0.36)",
   },
-
   reviewContainer: {
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "rgba(255,255,255,0.6)",
     borderRadius: scale(10),
     paddingHorizontal: scale(6),
-    paddingVertical: verticalScale(2),
+    paddingVertical: scale(2),
     gap: scale(4),
   },
-
   reviewText: {
-    fontSize: moderateScale(6),
+    fontSize: scale(6),
+    fontFamily: Family.FG_Medium,
   },
-
   iconCircle: {
     width: scale(12),
     height: scale(12),
