f863ed51679ca81703f17acdb20d8b81cbe5cf7e
mohidreekh <mohammad.idreekh00@gmail.com>
2026-03-27 16:52:11 +0200

    Refactoring my components to match different phone screen sizes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CaseCard from './src/components/common/CaseCard';
-import ViewMoreStatistics from './src/features/doctor/components/ViewMoreStatistics';
 import ActivityLogButton from './src/features/doctor/components/ActivityLogButton';
-import EntryScreen from './src/features/auth/EntryScreen';
 export default function App() {
   return (
     <View>
         <ActivityLogButton />
-        <EntryScreen />
     </View>
   );
 }
