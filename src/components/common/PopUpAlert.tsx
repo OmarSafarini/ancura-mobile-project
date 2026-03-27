aa988b1b8d579065d12f7d2912d9623612f0c33c
Omar Safarini <omarsafarini123344@gmail.com>
2026-03-27 12:59:04 +0200

    refactor the PopUpAleart component and doing some changes in the layout

--- src/components/common/PopUpAlert.tsx
+++ src/components/common/PopUpAlert.tsx
@@ -1,66 +1,41 @@
-import { Colors } from "@/utils/colors";
-import Svg, { Polygon } from "react-native-svg";
+import WarningIcon from "@/assets/icons/AlertIcon";
 import { Ionicons } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import { moderateScale, scale, verticalScale } from "react-native-size-matters";
+import { scale } from "@/utils/responsive";
+import { Colors } from "@/utils/colors";
+import { IPopUpAlert } from "@/types/IPopUpAlert";
+import { Family } from "@/utils/typography";
 
-type IPopUpAlert = {
-  title : string,
-  description : string,
-  cancel : string , 
-  confirm : string,
-  onConfirm?: () => void;
-  onCancel?: () => void;
-};
-
-export default function LogOutPopUp({ title, description ,cancel , confirm, onConfirm, onCancel }: IPopUpAlert) {
+export default function PopUpAlert({ title, description, cancel, confirm, onConfirm, onCancel }: IPopUpAlert) {
   return (
     <View style={styles.popup}>
-
       <View style={styles.iconContainer}>
-        <Svg width={scale(60)} height={scale(60)} viewBox="0 0 100 100">
-          <Polygon
-            points="30,5 70,5 95,30 95,70 70,95 30,95 5,70 5,30"
-            stroke={Colors.warning}
-            strokeWidth="5"
-            fill="transparent"
-          />
-        </Svg>
+        <WarningIcon size={60} />
         <Ionicons
           name="alert-outline"
-          size={moderateScale(28)}
+          size={scale(28)}
           color={Colors.warning}
           style={styles.icon}
         />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-
-      <Text style={styles.description}>
-        {description}
-      </Text>
+      <Text style={styles.description}>{description}</Text>
 
       <View style={styles.buttonRow}>
         <Pressable
-          style={({ pressed }) => [
-            styles.cancelBtn,
-            pressed && styles.buttonPressed,
-          ]}
+          style={({ pressed }) => [styles.cancelBtn, pressed && styles.buttonPressed]}
           onPress={onCancel}
         >
           <Text style={styles.cancelText}>{cancel}</Text>
         </Pressable>
         <Pressable
-          style={({ pressed }) => [
-            styles.logoutBtn,
-            pressed && styles.buttonPressed,
-          ]}
+          style={({ pressed }) => [styles.logoutBtn, pressed && styles.buttonPressed]}
           onPress={onConfirm}
         >
           <Text style={styles.logoutText}>{confirm}</Text>
         </Pressable>
       </View>
-
     </View>
   );
 }
@@ -68,25 +43,19 @@ export default function LogOutPopUp({ title, description ,cancel , confirm, onCo
 const styles = StyleSheet.create({
 
   popup: {
-    width: scale(220),
-    paddingVertical: verticalScale(20),
+    width: scale(254),
+    height: scale(235),
+    paddingVertical: scale(20),
     paddingHorizontal: scale(16),
-
-    backgroundColor: "#FFF",
+    backgroundColor: Colors.formBackground,
     borderRadius: scale(14),
-
-    /* border خفيف */
     borderWidth: 1,
     borderColor: "rgba(0,0,0,0.06)",
-
-    /* shadow احترافي */
-    shadowColor: "#000",
+    shadowColor: Colors.textDark,
     shadowOffset: { width: 0, height: 6 },
     shadowOpacity: 0.08,
     shadowRadius: 10,
-
-    elevation: 6, // Android shadow
-
+    elevation: 6,
     alignItems: "center",
   },
 
@@ -95,7 +64,7 @@ const styles = StyleSheet.create({
     height: scale(60),
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: verticalScale(12),
+    marginBottom: scale(12),
   },
 
   icon: {
@@ -103,53 +72,62 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    fontSize: moderateScale(18),
+    fontSize: scale(24),
     fontWeight: "600",
+    fontFamily : Family.FG_Medium,
     color: Colors.textDark,
-    marginBottom: verticalScale(6),
+    marginBottom: scale(6),
   },
 
   description: {
-    fontSize: moderateScale(11),
+    fontSize: scale(14),
     color: Colors.formLabel,
+    fontFamily : Family.FG_Regular,
     textAlign: "center",
-    marginBottom: verticalScale(18),
-    lineHeight: moderateScale(16),
+    marginBottom: scale(20),
+    marginTop: scale(10),
+    lineHeight: scale(16),
+    width : scale(160)
   },
 
   buttonRow: {
-    flexDirection: "row",
-    gap: scale(10),
-  },
+  flexDirection: "row",
+  justifyContent: "space-between",
+  width: "100%", 
+},
 
-  cancelBtn: {
-    backgroundColor: "#F0F0F0",
-    paddingVertical: verticalScale(7),
-    paddingHorizontal: scale(18),
-    borderRadius: scale(10),
-  },
+cancelBtn: {
+  width: "48%",
+  backgroundColor: "#F0F0F0",
+  paddingVertical: scale(7),
+  borderRadius: scale(10),
+  alignItems: "center", 
+  justifyContent: "center",
+},
 
-  logoutBtn: {
-    backgroundColor: Colors.warning,
-    paddingVertical: verticalScale(7),
-    paddingHorizontal: scale(18),
-    borderRadius: scale(10),
-  },
+logoutBtn: {
+  width: "48%",
+  backgroundColor: Colors.warning,
+  paddingVertical: scale(7),
+  borderRadius: scale(10),
+  alignItems: "center",
+  justifyContent: "center",
+},
 
   buttonPressed: {
     opacity: 0.75,
   },
 
   cancelText: {
-    fontSize: moderateScale(10),
+    fontSize: scale(10),
     color: Colors.textDark,
-    fontWeight: "500",
+    fontFamily : Family.FG_Regular,
   },
 
   logoutText: {
-    fontSize: moderateScale(10),
+    fontSize: scale(10),
+    fontFamily : Family.FG_Regular,
     color: "#FFF",
-    fontWeight: "500",
   },
 
 });
