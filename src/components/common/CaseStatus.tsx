e82999ba011219016192a1b2992cc6cecfc92df0
LianSx6 <s12323588@stu.najah.edu>
2026-03-31 23:15:21 +0300

    modified case status

--- src/components/common/CaseStatus.tsx
+++ src/components/common/CaseStatus.tsx
@@ -2,50 +2,21 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { palette,Colors } from "@/utils/colors";
 import { Family } from "@/utils/typography";
+import { scale } from "@utils/responsive";
+import { CaseStatusProps,Variant } from "@/types/ICaseStatusProps";
+import { caseStatusMap } from "@/types/ICaseStatusMap";
 
-import UnderReviewIcon from "@assets/icons/UnderViewIcon";
-import DoctorRepliedIcon from "@assets/icons/DoctorRepliedIcon";
-import ResolvedIcon from "@assets/icons/ResolvedIcon";
 
-const CaseStatus = ({ status, variant }: { status: string; variant?: "default" | "activityLog" }) => {
 
-  const getConfig = () => {
-    switch (status) {
-      case "under_review":
-        return {
-          text: "Under Review",
-          color: Colors.underReview,
-          Icon: UnderReviewIcon
-        };
+const CaseStatus = ({ status, variant="default" }: CaseStatusProps) => {
 
-      case "doctor_replied":
-        return {
-          text: "Doctor Replied",
-          color: Colors.secondary,
-          Icon: DoctorRepliedIcon
-        };
+  const config = caseStatusMap[status];
 
-      case "resolved":
-        return {
-          text: "Resolved",
-          color: Colors.secondary,
-          Icon: ResolvedIcon
-        };
-
-      default:
-        return {
-          text: status,
-          color: "#999",
-          Icon: null
-        };
-    }
-  };
-
-  const { text, color, Icon } = getConfig();
+  const { text, color, Icon } = config;
 
   return (
     <View style={[styles.container,variant === "activityLog" && styles.cardVariant]}>
-  {Icon && <Icon width={12} height={12} />}
+  {Icon && <Icon width={scale(12)} height={scale(12)} />}
 
   <Text style={[styles.text, { color }]}>
     {text}
@@ -64,17 +35,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
 
     backgroundColor: "#FFFFFF7A",    
-    borderRadius: 11,
+    borderRadius: scale(11),
 
-    width: 74,
-    height: 18.22,
+    width: scale(74),
+    height: scale(18.22),
 
-    gap: 2,
-    marginVertical: 4,
+    gap: scale(2),
+    marginVertical: scale(4),
   },
 
   text: {
-    fontSize: 8,
+    fontSize: scale(8),
     fontFamily: Family.FG_Regular,
   },
 
