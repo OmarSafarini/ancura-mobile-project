b3cb308af9dc83bd25a6bc053d98fcf46fa12db2
AhmadKhalil05 <s12323576@stu.najah.edu>
2026-04-09 00:49:40 +0300

    refactor the rules of password field to be min 8 and max 13

--- src/features/patient/screens/PatientAuthScreen.tsx
+++ src/features/patient/screens/PatientAuthScreen.tsx
@@ -78,7 +78,11 @@ export default function PatientAuthScreen() {
               name="password"
               label="Password"
               placeholder="Enter Password"
-              rules={{ required: "Password is required" }}
+              rules={{ 
+                required: "Password is required",
+                minLength: { value: 8, message: "Password must be at least 8 characters" },
+                maxLength: { value: 13, message: "Password cannot exceed 13 characters" }
+              }}
               secureTextEntry={true}
             />
 
