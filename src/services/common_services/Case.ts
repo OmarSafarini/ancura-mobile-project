7681dd9c5b42e884ad0d775acc2e1045f629bc17
LianSx6 <s12323588@stu.najah.edu>
2026-04-15 21:44:32 +0300

    modified the dashboard pages

---
+++ src/services/common_services/Case.ts
@@ -0,0 +1,27 @@
+import { supabaseClient } from '../supabase';   
+
+export const getAllCases = async () => {
+  try {
+    const { data, status, statusText } = await supabaseClient.get('/post', {
+      params: {
+        select: '*',
+
+        isreplied: 'eq.false',
+
+        order: 'timestamp.desc',
+      },
+      headers: {
+      },
+    });
+
+    console.log('Status:', status, statusText);
+    console.log('Fetched cases:', data);
+    console.log('Data status:', data.status);
+
+    return Array.isArray(data) ? data : [];
+  } catch (error: any) {
+    console.error('Error fetching cases:', error.response?.data || error.message);
+    console.error('Full error:', error);
+    throw error;
+  }
+};
