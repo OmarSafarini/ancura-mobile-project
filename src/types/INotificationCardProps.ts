2dd6885e4e1562c170fafa35094a330d54f0a83e
Omar Safarini <omarsafarini123344@gmail.com>
2026-05-11 19:25:02 +0300

    adding the notification hook

--- src/types/INotificationCardProps.ts
+++ src/types/INotificationCardProps.ts
@@ -1,6 +1,9 @@
-export interface INotificationCardProps {
+export interface INotification {
+    id: number;          
+    patient_id?: string;  
     title: string;
-    date: string;
+    date: string;         
     isRead: boolean;
-    status: string;
+    status: 'None' | 'Success' | 'Warning' | 'Error' | string; 
+    created_at?: string;  
 }
