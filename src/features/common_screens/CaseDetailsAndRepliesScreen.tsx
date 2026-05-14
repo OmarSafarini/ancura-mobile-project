7da6bb559a99e45338b9abd49f5128ca27054f6c
LianSx6 <s12323588@stu.najah.edu>
2026-05-14 21:16:11 +0300

    fix the scrolling of the reply cards

--- src/features/common_screens/CaseDetailsAndRepliesScreen.tsx
+++ src/features/common_screens/CaseDetailsAndRepliesScreen.tsx
@@ -281,8 +281,9 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingBottom: scale(30),
     paddingHorizontal: scale(24),
-    position: "absolute",
-    bottom: scale(30),
+    // position: "absolute",
+    // bottom: scale(30),
+   
   },
 
   patientBottom: {
